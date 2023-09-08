5860cc38d6673f9b810ca796e02d110df7e505ff
sebasrmusik <72873947+sebasrmusik@users.noreply.github.com>
2023-09-08 18:49:51 -0500

    Feat: Agregada funcionalidad de agregar usuarios

--- src/components/modals/users/AgregarUsuario.jsx
+++ src/components/modals/users/AgregarUsuario.jsx
@@ -1,10 +1,29 @@
 import { useState, useEffect } from "react"
 import './AgregarUsuario.css'
 
-function AgregarUsuario () {
+function AgregarUsuario ({agregarUsuarioModal, setDataParent}) {
+
     const [sedes, setSedes] = useState({})
     const [dataReceived, setDataReceived] = useState(false)
     const accessToken = localStorage.getItem('accessToken');
+
+    const [values, setValues] = useState({
+        password: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        deleted_at: null,
+        salario: 0,
+        cargo: null,
+        groups: [],
+        sede: '',
+    })
+
+    const inputHandler = (name) => {
+        return (e) => {
+            setValues({...values, [name] : e.target.value})
+        }
+    }
 
     const togglePassword = () => {
         const togglePassButton = document.querySelector('#togglePassword')
@@ -24,13 +43,54 @@ function AgregarUsuario () {
         return jsonResponse
     }
 
-    useEffect(() => {
-        sedeRequest()
-            .then((data) => {
-                setSedes(data)
-                setDataReceived(true)
+    const agregarUsuario = async () => {
+        console.log(values);
+        const response = await fetch('https://waretrack-api.onrender.com/funcionario/', {
+            mode: 'cors',
+            method: 'post',
+            headers: {'Authorization': `Bearer ${accessToken}`, 'Content-type': 'application/json'},
+            body: JSON.stringify({
+                ...values,
+                sede: [values.sede],
+                salario: Number(values.salario)
             })
-    }, [])
+        })
+        return response
+    }
+
+    const addUser = () => {
+        agregarUsuario()
+            .then((response) => {
+                document.querySelector('#closeAgregarUsuario').click()
+                setDataParent(false)
+            })
+            .catch((e) => console.log(e))
+    }
+
+    const limpiarFormulario = () => {
+        setValues({
+            password: '',
+            first_name: '',
+            last_name: '',
+            email: '',
+            deleted_at: null,
+            salario: 0,
+            cargo: null,
+            groups: [],
+            sede: '',
+        })
+    }
+
+    useEffect(() => {
+        console.log(agregarUsuarioModal);
+        if(agregarUsuarioModal){
+            sedeRequest()
+                .then((data) => {
+                    setSedes(data)
+                    setDataReceived(true)
+                }) 
+        }
+    }, [agregarUsuarioModal])
     
     return <div className="modal fade" id="modal-agregar-usuario" tabIndex="-1" role="dialog" aria-labelledby="modal-agregar-usuario" aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -46,13 +106,13 @@ function AgregarUsuario () {
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="first-name" className="fs-6 pt-1">Nombres</label>
-                                            <input className="form-control w-65" type="text" name="first-name" id="first-name" required/>
+                                            <input value={values["first_name"]} className="form-control w-65" type="text" name="first-name" id="first-name" required onChange={inputHandler("first_name")}/>
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="last-name" className="fs-6 pt-1">Apellidos</label>
-                                            <input className="form-control w-65" type="text" name="last-name" id="last-name" required/>
+                                            <input value={values["last_name"]} className="form-control w-65" type="text" name="last-name" id="last-name" required onChange={inputHandler("last_name")}/>
                                         </div>
                                     </div>
                                 </div>
@@ -60,14 +120,14 @@ function AgregarUsuario () {
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="email" className="fs-6 pt-1">Email</label>
-                                            <input className="form-control w-65" type="email" name="email" id="email" required/>
+                                            <input value={values["email"]} className="form-control w-65" type="email" name="email" id="email" required onChange={inputHandler("email")}/>
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="password" className="fs-6 pt-1">Contraseña</label>
                                             <div className="input-group w-65 ">
-                                                <input className="form-control" type="password" name="password" id="password" required/>
+                                                <input value={values["password"]} className="form-control" type="password" name="password" id="password" required onChange={inputHandler("password")}/>
                                                 <button className="btn btn-outline-primary mb-0 fs-5 px-3 py-0" type="button" id="togglePassword" onClick={togglePassword}>
                                                     <i className="bi bi-eye-slash" id=""></i>
                                                 </button> 
@@ -79,11 +139,11 @@ function AgregarUsuario () {
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="sede" className="fs-6 pt-1">Sede</label>
-                                            <select name="sede" id="sede" className="form-select w-65" required>
+                                            <select value={values["sede"]} name="sede" id="sede" className="form-select w-65" required onChange={inputHandler("sede")}>
                                                 <option value="">Selecciona una opción</option>
                                                 {dataReceived ? 
                                                     sedes.map((sede) => {
-                                                        return <option key={sede["id"]} value={sede["id"]}>{`${sede["id"]} - ${sede["nombre"]}`}</option>
+                                                        return <option key={sede["id"]} value={sede["id"]}>{sede["nombre"]}</option>
                                                     })
                                                 : <option value="">Loading...</option>}
                                             </select>
@@ -92,27 +152,31 @@ function AgregarUsuario () {
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
                                             <label htmlFor="salario" className="fs-6 pt-1">Salario</label>
-                                            <input className="form-control w-65" type="number" name="salario" id="salario" min="0" required onWheel={ event => event.currentTarget.blur() }/>
+                                            <input value={values["salario"]} className="form-control w-65" type="number" name="salario" id="salario" min="0" required onWheel={ event => event.currentTarget.blur() } onChange={inputHandler("salario")}/>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-6">
                                         <div className="d-flex gap-2 align-items-center justify-content-between">
-                                            <label htmlFor="sede" className="fs-6 pt-1">Cargo</label>
-                                            <select name="sede" id="sede" className="form-select w-65" required>
+                                            <label htmlFor="cargo" className="fs-6 pt-1">Cargo</label>
+                                            <select value={values["cargo"]} name="cargo" id="cargo" className="form-select w-65" required onChange={inputHandler("cargo")}>
                                                 <option value="">Selecciona una opción</option>
                                                 
                                             </select>
                                         </div>
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="d-flex justify-content-end">
-                                            <button className="btn btn-primary" type="button">Agregar</button>
-                                        </div>
+                                        
                                     </div>
                                 </div>
                             </form>
+                        </div>
+                        <div className="card card-footer">
+                            <div className="d-flex justify-content-end gap-2">
+                                <button className="btn btn-secondary" type="button" data-bs-dismiss="modal" id="closeAgregarUsuario" onClick={limpiarFormulario}>Cancelar</button>
+                                <button className="btn btn-primary" type="button" onClick={addUser}>Agregar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/views/Usuarios.jsx
+++ src/pages/views/Usuarios.jsx
@@ -3,57 +3,60 @@ import DataTable from 'react-data-table-component';
 import { PageTitle } from '../../App'
 import AgregarUsuario from '../../components/modals/users/AgregarUsuario'
 
-const columns = [
-    {
-        name: 'Nombres',
-        selector: row => row.first_name,
-        sortable: true
-    },
-    {
-        name: 'Apellidos',
-        selector: row => row.last_name,
-        sortable: true
-    },
-    {
-        name: 'Cargo',
-        selector: row => row.cargo,
-        sortable: true
-    },
-    {
-        name: 'Sede',
-        selector: row => row.sede,
-        sortable: true
-    },
-    {
-        name: 'Email',
-        selector: row => row.email,
-    },
-    {   
-        name: 'Acciones',
-        cell: () => (
-            <div className="App d-flex justify-content-center pt-3 pb-0">
-                <div className="btn-group">
-                    <button className="btn btn-outline-secondary py-2 px-3">
-                        <i className="bi bi-pencil fs-5"></i>
-                    </button>
-                    <button className="btn btn-outline-secondary py-2 px-3">
-                        <i className="bi bi-trash fs-5"></i>
-                    </button>
-                </div>
-            </div>
-        )
-    }
-]
 
 function Usuarios () {
     const [usuarios, setUsuarios] = useState([])
+    const [agregarUsuarioModal, setAgregarUsuarioModal] = useState(false)
     const [dataReceived, setDataReceived] = useState(false)
+    const [updateUsuario, setUpdateUsuario] = useState(null)
     const {setTitle} = useContext(PageTitle);
     const accessToken = localStorage.getItem('accessToken');
-
+    
+    const columns = [
+        {
+            name: 'Nombres',
+            selector: row => row.first_name,
+            sortable: true
+        },
+        {
+            name: 'Apellidos',
+            selector: row => row.last_name,
+            sortable: true
+        },
+        {
+            name: 'Cargo',
+            selector: row => row.cargo,
+            sortable: true
+        },
+        {
+            name: 'Sede',
+            selector: row => row.sede,
+            sortable: true
+        },
+        {
+            name: 'Email',
+            selector: row => row.email,
+        },
+        {   
+            name: 'Acciones',
+            cell: row => {
+                return (
+                <div className="App d-flex justify-content-center pt-3 pb-0">
+                    <div className="btn-group">
+                        <button className="btn btn-outline-secondary py-2 px-3" onClick={() => updateUser(row)}>
+                            <i className="bi bi-pencil fs-5"></i>
+                        </button>
+                        <button className="btn btn-outline-secondary py-2 px-3" onClick={() => deleteUser(row)}>
+                            <i className="bi bi-trash fs-5"></i>
+                        </button>
+                    </div>
+                </div>
+            )}
+        }
+    ]
+    
+    
     const usersRequest = async () => {
-        
-        
         const response = await fetch('https://waretrack-api.onrender.com/funcionario/', {
             mode: 'cors',
             method: 'get',
@@ -62,15 +65,24 @@ function Usuarios () {
         const jsonResponse = await response.json()
         return jsonResponse
     }
-
-     useEffect(() => {
+    
+    useEffect(() => {
         setTitle('Usuarios');
         usersRequest()
-            .then((data) => {
-                setUsuarios(data)
-                setDataReceived(true);
-            })
-     }, [])
+        .then((data) => {
+            setUsuarios(data)
+            setDataReceived(true);
+        })
+    }, [dataReceived])
+    
+    const updateUser = (usuario) => {
+        setUpdateUsuario(usuario)
+        console.log(updateUsuario);
+    }
+
+    const deleteUser = (usuario) => {
+        console.log(usuario);
+    }
 
     return <>
     <div className="row">
@@ -78,11 +90,8 @@ function Usuarios () {
             <div className="card mb-4">
                 <div className="card-header pb-0 d-flex justify-content-between" >
                     <h6>Usuarios</h6>
-                    <button className="btn btn-secondary d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#modal-agregar-usuario">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person-plus" viewBox="0 0 16 16">
-                            <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                            <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
+                    <button className="btn btn-secondary d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#modal-agregar-usuario" onClick={() => setAgregarUsuarioModal(true)}>
+                    <i class="bi bi-person-add fs-5 py-0"></i>
                          Agregar usuario 
                     </button>
                 </div>
@@ -93,14 +102,14 @@ function Usuarios () {
                             columns={columns}
                             data={usuarios}
                             pagination
-                        /> 
+                            /> 
                         : <h5 className="ps-5">Loading Data</h5>}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <AgregarUsuario />
+    <AgregarUsuario agregarUsuarioModal={agregarUsuarioModal} setDataParent={setDataReceived}/>
     </>
 }
 
