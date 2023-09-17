61dd824c2a0bb17368031dea7ee4eb08048456e3
Ana Mendez <ana.mendezr@uniagustiniana.edu.co>
2023-09-16 22:02:51 -0500

    Refactor - Módulo de Proveedores y Pedidos estilos

---
+++ src/containers/CarrouselProveedores.jsx
@@ -0,0 +1,171 @@
+const CarrouselProveedores = () => {
+    return <>
+        <div className="row">
+            <div id="carousel" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    {/* Acá deben mostrarse los primeros tres proveedores, ya que por la clase "active" es lo que se mostrará siempre */}
+                    <div className="carousel-item active">
+                        <div className="d-flex flex-row gap-2">
+
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    {/* Acá debe mostrarse luego del proveedor 3 */}
+                    <div className="carousel-item">
+                        <div className="d-flex flex-row gap-2">
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div className="card">
+                                    <div className="card-body p-3">
+                                        <div className="row">
+                                            <div className="col-8">
+                                                <div className="numbers">
+                                                    <p className="text-sm mb-0 text-capitalize font-weight-bold">correoproveedor@gmail.com</p>
+                                                    <h5 className="font-weight-bolder mb-0">
+                                                        Nombre proveedor
+                                                    </h5>
+                                                    {/* <span className="text-success text-sm font-weight-bolder">+55%</span> */}
+                                                </div>
+                                            </div>
+                                            <div className="col-4 text-end  d-flex justify-content-center align-items-center">
+                                                <div className="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                                    <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default CarrouselProveedores;
--- src/pages/views/PedidosProveedores.jsx
+++ src/pages/views/PedidosProveedores.jsx
@@ -1,177 +1,18 @@
 import { useEffect, useContext } from "react"
 import { PageTitle } from "../../App"
+import CarrouselProveedores from "../../containers/CarrouselProveedores.jsx"
+import Pedidos from "../../containers/Pedidos.jsx"
 
-function PedidosProveedores () {
-    const {setTitle} = useContext(PageTitle)
+function PedidosProveedores() {
+    const { setTitle } = useContext(PageTitle)
 
     useEffect(() => {
         setTitle('Pedidos - Proveedores')
     })
-    
+
     return <>
-    <div className="row">
-        <div id="carousel" className="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">  
-                    <div className="d-flex flex-row gap-2">
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="d-flex flex-row gap-2">
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card w-25">
-                            <div className="card-body p-3">
-                                <div className="row">
-                                    <div className="col-8">
-                                        <h5 className="font-weight-bolder">PIÑONES LTDA  
-                                        </h5>
-                                        <p className="text-sm font-weight-bold">correo</p>
-                                        <span className="text-sm font-weight-bold">3001234567</span>
-                                    </div>
-                                    <div className="col-4">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-
-                    </div>
-                </div>
-                
-                <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-    </div>
-    <div className="row">
-        <div className="col md-7 mt-4">
-            <div className="card">
-                <div className="card-body">
-                    Card
-                </div>
-            </div>
-        </div>
-        <div className="col md-5 mt-4">
-            <div className="card">
-                <div className="card-body">
-                    Card
-                </div>
-            </div>
-        </div>
-    </div>
+        <CarrouselProveedores/>
+        <Pedidos/>
     </>
 }
 
